3e52f8880aeb371fa759262bc8a65aa76d58cc23
novysvet <anotherdimension@proton.me>
2026-08-28 15:48:35 +0200

    Target Raptor Lake host ISA

--- build/glm53-gen.sh
+++ build/glm53-gen.sh
@@ -5,7 +5,8 @@ repo="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 out="${INSIGNIA_BUILD_DIR:-/var/tmp/insignia-build}"
 mkdir -p "$out"
 exec /opt/cuda/bin/nvcc -ccbin /usr/bin/g++-15 -arch=sm_89 -O3 --use_fast_math -lineinfo \
-    -Xcompiler=-pthread -std=c++20 -DINSIGNIA_GLM53_NO_MAIN -I"$repo/include" \
+    -Xcompiler=-pthread -Xcompiler=-march=raptorlake -Xcompiler=-mtune=raptorlake \
+    -std=c++20 -DINSIGNIA_GLM53_NO_MAIN -I"$repo/include" \
     "$repo/src/bf16.cu" "$repo/src/glm53_expert_bench.cu" "$repo/src/glm53_ops.cu" \
     "$repo/src/glm53_q8.cu" "$repo/src/glm53_fp8.cu" "$repo/src/glm53_dflash2.cu" \
     "$repo/src/glm53_generate.cu" \
